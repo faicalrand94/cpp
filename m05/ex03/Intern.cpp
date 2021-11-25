f191b6ce504523d01cbeaf825fba5a7f8cbcd905
Faical Bouibaouen <fbouibao@e1r5p2.1337.ma>
2021-11-25 05:22:03 +0100

    hh

--- m05/ex03/Intern.cpp
+++ m05/ex03/Intern.cpp
@@ -6,37 +6,60 @@
 /*   By: fbouibao <fbouibao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 10:12:11 by fbouibao          #+#    #+#             */
-/*   Updated: 2021/11/24 10:15:41 by fbouibao         ###   ########.fr       */
+/*   Updated: 2021/11/25 05:21:25 by fbouibao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "PresidentialPardonForm.hpp"
-#include "RobotomyRequestForm.hpp"
+
 #include "Intern.hpp"
 
 Intern::Intern()
 {
-    
-}
-
-Intern::Intern(std::string target)
-{
-    
+    std::cout << "Intern Default constructor called" << std::endl;
 }
 
 Intern::~Intern()
 {
-    
+    std::cout << "Intern destructor called" << "\n";
 }
+
 Intern::Intern(const Intern &f)
 {
-    
+    std::cout << "Intern Copy constructor called" << std::endl;
 }
+
 void Intern::operator=(const Intern &f)
 {
-    
+    std::cout << "Intern assignment operateur called" << std::endl;
 }
+
+PresidentialPardonForm *ppf(std::string target)
+{
+    return (new PresidentialPardonForm(target));
+}
+
+RobotomyRequestForm *rrf(std::string target)
+{
+    return (new RobotomyRequestForm(target));
+}
+
+ShrubberyCreationForm *scf(std::string target)
+{
+    return (new ShrubberyCreationForm(target));
+}
+
 Form* Intern::makeForm(std::string nameForm, std::string target)
 {
+    
+    std::string forms[3] = {"presidential pardon" , "robotomy request", "shrubbery creation"};
+    Form* (*ptr[3])();
+    ptr[0] = ppf;
+    ptr[1] = rrf;
+    ptr[0] = scf;
+    for (int i = 0; i < 3; i++)
+    {
+        if (nameForm == forms[i])
+            return (pt[i]);
+    }
     
 }
--- m05/ex03/Intern.hpp
+++ m05/ex03/Intern.hpp
@@ -6,7 +6,7 @@
 /*   By: fbouibao <fbouibao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 09:36:28 by fbouibao          #+#    #+#             */
-/*   Updated: 2021/11/24 10:10:18 by fbouibao         ###   ########.fr       */
+/*   Updated: 2021/11/25 05:01:47 by fbouibao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,14 @@
 #include <iostream>
 #include <exception>
 #include <fstream>
-#include "Form.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
 
 class Intern : public Form
 {
     public:
         Intern();
-        Intern(std::string target);
         ~Intern();
         Intern(const Intern &f);
         void operator=(const Intern &f);
