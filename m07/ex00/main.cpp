f3df1118a0f55275dbc036c981a27f913ad84a80
Faical Bouibaouen <fbouibao@e1r5p2.1337.ma>
2021-12-04 11:38:14 +0100

    ex01 m07 c++

--- m07/ex00/main.cpp
+++ m07/ex00/main.cpp
@@ -2,23 +2,18 @@
 
 int main()
 {
-    int x = 10;
-    int y = 20;
-    std::cout << "before swap : x ==> " << x << " y ==> " << y << std::endl;
-    std::cout << "min ==> " << min(x, y) << std::endl;
-    std::cout << "max ==> " << max(x, y) << std::endl;
-    swap(x, y);
-    std::cout << "after swap : x ==> " << x << " y ==> " << y << std::endl;
+    int a = 2;
+    int b = 3;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 
-
-    std::cout << "\n**********************************\n\n";
-
-    char a = 'A';
-    char z = 'Z';
-    std::cout << "before swap : a ==> " << a << " z ==> " << z << std::endl;
-    std::cout << "min ==> " << min(a, z) << std::endl;
-    std::cout << "max ==> " << max(a, z) << std::endl;
-    swap(a, z);
-    std::cout << "after swap : a ==> " << a << " z ==> " << z << std::endl;  
     return (0);
 }
---
+++ m07/ex01/iter.hpp
@@ -0,0 +1,15 @@
+#ifndef  IETR_HPP
+#define IETR_HPP
+
+#include <iostream>
+
+template<typename iter>
+void update(iter *ar, int length, void (f)(iter &))
+{
+    for (int i = 0; i < length; i++)
+    {
+        f(ar[i]);
+    }
+}
+
+#endif
